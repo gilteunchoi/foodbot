292006d1a265cac24814e984448db5a85c20a4ce
Gilteun Choi <gilteunchoi@gmail.com>
2022-03-31 00:01:06 +0900

    helptext

--- index.js
+++ index.js
@@ -5,11 +5,14 @@ const prefix = '!';
 const helptext = `\`\`\`실시간예약봇 | 맛집 예약을 위한 실시간 인스타 크롤러 1.0.0-alpha
 
 [명령어 목록]
-!ping - Test
-!text - text
+!ping - pong
+!help - 도움말 보기
 
 [주시중인 맛집 목록]
-test\`\`\``
+스시심타카이 (@sushi_sim)
+
+[소스코드 보기]
+https://github.com/gilteunchoi/foodbot\`\`\``
 
 const client = new Discord.Client({
     intents: [
